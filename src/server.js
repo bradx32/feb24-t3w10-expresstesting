4b1f1ec1617c44a58d74227008c3414d302c8eab
Bradley Richmond <bradx32@hotmail.com>
2024-10-19 09:10:01 +1000

    feat: project init with hello world route

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+// Make an instance of the Express server system
+// so that we can configure it 
+const app = express();
+
+// localhost:3000/
+// domainName:port/
+// serverInstance.verb(path, callback)
+app.get("/", (request, response) => {
+	response.json({
+		message:"Hello world!"
+	});
+});
+
+module.exports = {
+	app
+}
+
+// Different export syntax: 
+// module.exports = app;
---
+++ tests/server.test.js
@@ -0,0 +1,9 @@
+const {app} = require("../src/server.js");
+
+describe("Root route", () => {
+	test("Server returns a 'Hello world!' message", async () => {
+		// make a request to "/" route 
+
+		// check the body of the response for a "Hello world!" string 
+	});
+});
